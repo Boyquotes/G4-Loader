d5f8cfe9ac5475ad9124dd5882f266383c76c5f7
odysz <odysseusj@163.com>
2020-01-16 23:30:55 +0800

    create gdx project: basics

---
+++ test/gdx/android/src/io/oz/g4/proto/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package io.oz.g4.proto;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.oz.g4.proto.Proto1;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Proto1(), config);
+	}
+}
---
+++ test/gdx/desktop/src/io/oz/g4/proto/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.oz.g4.proto.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.oz.g4.proto.Proto1;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Proto1(), config);
+	}
+}
